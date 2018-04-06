23c83694ca3d923656715f6581c6080c7fa44aea
Morgan Robertson <>
2018-04-07 02:42:59 +1000

    Add Enemy class and random spawn

---
+++ LittleTiggy/Enemy.cs
@@ -0,0 +1,152 @@
+﻿using System;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input.Touch;
+using Microsoft.Xna.Framework.Input;
+
+
+
+namespace LittleTiggy
+{
+
+    public class Enemy
+    {
+        static Texture2D characterSheetTexture;
+        Animation walkDown;
+        Animation walkLeft;
+        Animation walkRight;
+        Animation walkUp;
+        Animation standDown;
+        Animation standLeft;
+        Animation standRight;
+        Animation standUp;
+        Animation Idle;
+        Animation currentAnimation;
+
+        const float charSpeed = 0.00001F;
+        //long lastCountOfTicks = 0;
+        long ticksSinceLastUpdate = 0;
+
+
+        public float X
+        {
+            get;
+            set;
+        }
+
+        public float Y
+        {
+            get;
+            set;
+        }
+
+        public Enemy(GraphicsDevice graphicsDevice, EnvironmentBlock[] walls)
+        {
+            if (characterSheetTexture == null)
+            {
+                using (var stream = TitleContainer.OpenStream("Content/charactersheet.png"))
+                {
+                    characterSheetTexture = Texture2D.FromStream(graphicsDevice, stream);
+                }
+            }
+
+            walkDown = new Animation();
+            walkDown.AddFrame(new Rectangle(0, 0, 16, 16), TimeSpan.FromSeconds(.25));
+            walkDown.AddFrame(new Rectangle(16, 0, 16, 16), TimeSpan.FromSeconds(.25));
+            walkDown.AddFrame(new Rectangle(0, 0, 16, 16), TimeSpan.FromSeconds(.25));
+            walkDown.AddFrame(new Rectangle(32, 0, 16, 16), TimeSpan.FromSeconds(.25));
+
+            walkLeft = new Animation();
+            walkLeft.AddFrame(new Rectangle(48, 0, 16, 16), TimeSpan.FromSeconds(.25));
+            walkLeft.AddFrame(new Rectangle(64, 0, 16, 16), TimeSpan.FromSeconds(.25));
+            walkLeft.AddFrame(new Rectangle(48, 0, 16, 16), TimeSpan.FromSeconds(.25));
+            walkLeft.AddFrame(new Rectangle(80, 0, 16, 16), TimeSpan.FromSeconds(.25));
+
+            walkRight = new Animation();
+            walkRight.AddFrame(new Rectangle(96, 0, 16, 16), TimeSpan.FromSeconds(.25));
+            walkRight.AddFrame(new Rectangle(112, 0, 16, 16), TimeSpan.FromSeconds(.25));
+            walkRight.AddFrame(new Rectangle(96, 0, 16, 16), TimeSpan.FromSeconds(.25));
+            walkRight.AddFrame(new Rectangle(128, 0, 16, 16), TimeSpan.FromSeconds(.25));
+
+            walkUp = new Animation();
+            walkUp.AddFrame(new Rectangle(144, 0, 16, 16), TimeSpan.FromSeconds(.25));
+            walkUp.AddFrame(new Rectangle(160, 0, 16, 16), TimeSpan.FromSeconds(.25));
+            walkUp.AddFrame(new Rectangle(144, 0, 16, 16), TimeSpan.FromSeconds(.25));
+            walkUp.AddFrame(new Rectangle(176, 0, 16, 16), TimeSpan.FromSeconds(.25));
+
+            Idle = new Animation();
+            Idle.AddFrame(new Rectangle(0, 0, 16, 16), TimeSpan.FromSeconds(.25));
+
+            standDown = new Animation();
+            standDown.AddFrame(new Rectangle(0, 0, 16, 16), TimeSpan.FromSeconds(.25));
+
+            standLeft = new Animation();
+            standLeft.AddFrame(new Rectangle(48, 0, 16, 16), TimeSpan.FromSeconds(.25));
+
+            standRight = new Animation();
+            standRight.AddFrame(new Rectangle(96, 0, 16, 16), TimeSpan.FromSeconds(.25));
+
+            standUp = new Animation();
+            standUp.AddFrame(new Rectangle(144, 0, 16, 16), TimeSpan.FromSeconds(.25));
+
+            currentAnimation = Idle;
+
+            Random randomNumber = new Random();
+            do
+            {
+                this.X = (float)randomNumber.Next(0, 512);
+                this.Y = randomNumber.Next(0, 512);
+
+                this.X -= this.X % 16;
+                this.Y -= this.Y % 16;
+
+            } while (IsEnvironmentCollision(walls));
+
+        }
+
+        public void Update(GameTime gameTime, GraphicsDevice graphicsDevice, EnvironmentBlock[] walls)
+        {
+            ticksSinceLastUpdate = gameTime.ElapsedGameTime.Ticks;
+
+            float lastXPosition = this.X;
+            float lastYPosition = this.Y;
+
+            //check collisions with environment walls
+
+            currentAnimation.Update(gameTime);
+
+        }
+
+        public void Draw(SpriteBatch spriteBatch)
+        {
+            Vector2 topLeftOfSprite = new Vector2(this.X, this.Y);
+            Color tintColor = Color.White;
+            var sourceRectangle = currentAnimation.CurrentRectangle;
+
+            spriteBatch.Draw(characterSheetTexture, topLeftOfSprite, sourceRectangle, tintColor);
+
+        }
+
+        bool IsEnvironmentCollision(EnvironmentBlock[] walls)
+        {
+            for (int i = 0; i < walls.Length; i++)
+            {
+                Rectangle wall = new Rectangle((int)walls[i].X, (int)walls[i].Y, 16, 16);
+                Rectangle character = new Rectangle((int)this.X, (int)this.Y, 16, 16);
+
+                if (character.Intersects(wall))
+                {
+                    return true;
+                }
+
+            }
+
+            return false;
+
+        }
+
+
+
+
+    }
+}
